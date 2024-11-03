a46a736ed9c24e8848947c248f30e6bfffdd8409
AmirhosseinKaviani <amirhk120@gmail.com>
2024-11-02 23:34:16 -0700

    supplement Blog page

--- react-blog-website/src/pages/components/BlogCards.jsx
+++ react-blog-website/src/pages/components/BlogCards.jsx
@@ -1,17 +1,21 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
+import { FaUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom'
 
 
-const BlogCards = ({blogs}) => {
-    const filteredBlogs =blogs;
-    console.log(filteredBlogs)
+const BlogCards = ({blogs,currentPage, selectedCategory, pageSize}) => {
+    const filteredBlogs = blogs.filter((blog) => !selectedCategory || blog.category === selectedCategory).slice((currentPage-1) * pageSize, (currentPage) * pageSize);
   return (
-   <div>
+   <div className='lg:mx-14 mx-10 mt-5 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8'>
     {
-        filteredBlogs.map((blog) => <Link key={blog.id}>
+        filteredBlogs.map((blog) => <Link key={blog.id} className='shadow-lg p-4 rounded cursor-pointer'>
             <div>
-                <img src={blog.image} alt='#' />
+                <img src={blog.image} alt='#' className='w-full'/>
             </div>
+            <h3 className='mt-4 mb-2 font-bold hover:text-blue-600 cursor-pointer'>{blog.title}</h3>
+            <p className='mb-2 flex items-center gap-3 text-gray-500'><FaUser className=''/>{blog.author}</p>
+            <p className='text-sm text-gray-500'>Published: {blog.published_date}</p>
         </Link>)
     }
    </div>
--- react-blog-website/src/pages/components/BlogPage.jsx
+++ react-blog-website/src/pages/components/BlogPage.jsx
@@ -4,20 +4,33 @@ import BlogCards from './BlogCards'
 
 const BlogPage = () => {
   const [blogs, setBlogs] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1)
+  const pageSize=12
+  const [selectedCategory, setSelectedCategory] = useState(null)
+  const [activeCategory, setActiveCategory] = useState(null)
+
   useEffect(()=>{
     async function fetchBlogs() {
-      let url = `http://localhost:5000/blogs`;
+      let url = `http://localhost:5000/blogs?page=${currentPage}&limit=${pageSize}`;
       const response = await fetch(url)
       let data = await response.json()
       setBlogs(data)
     }
     fetchBlogs()
-  },[])
+  },[currentPage, pageSize, selectedCategory])
+  const handlePageChange =(pageNumber) => {
+    setCurrentPage(pageNumber)
+  }
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category)
+    setCurrentPage(1)
+    setActiveCategory(category)
+
+  }
 
   return (
     <div>
-      <div>Page Category</div>
-      <div><BlogCards blogs={blogs}/></div>
+      <div><BlogCards blogs={blogs} currentPage={currentPage} selectedCategory={selectedCategory} pageSize={pageSize}/></div>
       <div>Pagination</div>
     </div>
   )
